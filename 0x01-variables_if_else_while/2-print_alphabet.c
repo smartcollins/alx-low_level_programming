9055a8fe8ea7375d0d2f92b2f64c7a85c8b4487c
collins <kevin1520smart@gmail.com>
2023-02-17 15:33:12 -0800

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints the alphabet.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghiklmnopqrstuvwqyz";
+	int i;
+
+	for (1 = 0; 1 < 26; 1tt)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
