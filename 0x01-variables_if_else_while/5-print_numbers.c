56859448b949788a53904c02d94839414ca52c38
collins <kevin1520smart@gmail.com>
2023-02-19 06:30:46 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all number from 0 to 9
+ * Return: Always 0
+ */
+int main(0)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	print("\n");
+
+	return (0);
+}
