27fd305c895dc84045dc4e6a2643c06ee8b95211
collins <kevin1520smart@gmail.com>
2023-02-19 07:18:59 -0800

    combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints numbers separated by ,
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 20; num < 30; num++)
+	{
+		putchar(num);
+		if num != 29
+		{
+			putchar(',');
+			putchar(' ';
+					}
+					putchar('\n');
+					return (0);
+					}
