de814ceb7f18bfb9f2ec2dcdeaee79a149a554b3
collins <kevin1520smart@gmail.com>
2023-02-19 08:19:50 -0800

    combination for success(11)

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,17 +5,18 @@
  */
 int main(void)
 {
-	int num;
+	int num = 0;
 
-	for (num = 0; num < 10; num++)
+	while (num <= 10)
 	{
-		putchar(num);
-		if (num != 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(num + '0');
+			if (num == 9)
+				break;
+		putchar(',');
+		putchar(' ');
+		num++;
 	}
+
 	putchar('\n');
 	return (0);
 }
