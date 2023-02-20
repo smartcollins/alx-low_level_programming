8518ef77f21991b22b9a98617ccca0c8986d8788
collins <kevin1520smart@gmail.com>
2023-02-20 07:29:39 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints a text followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf('\'with proper grammar, but the outcome is a piece of art,\n');
+	return (0);
+}
