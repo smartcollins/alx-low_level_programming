b6e1205e2fab7b58a3875a8b38a9d77f951403e3
collins <kevin1520smart@gmail.com>
2023-02-22 13:17:48 -0800

    isLower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * int - checks for lowercase characterby using ASCII code
+ * Return: 1 for lowercase and 0 for others
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
