9fe570ce5e360df194c3ed8b58751469d0e39b80
collins <kevin1520smart@gmail.com>
2023-02-22 14:04:35 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - checks for lower or upppercase letter
+ * @c: walue gotten
+ * Return: Always 1 if _isaplha true else 0
+*/
+int _isalpha(int c)
+{
+	if (c >= 'a' || c >= 'A' && c <= 'z' || c <= 'Z')
+		return (1)
+	else
+		return (0)
+}
