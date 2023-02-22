b78e829cc312a250e7b2be83552bdc7dd4c48511
collins <kevin1520smart@gmail.com>
2023-02-22 12:18:36 -0800

    work in progress

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,18 @@
-#include <stdio.h>
+#include "main.h" 
 /**
  * main - prints putchar
  * Return: Always 0
  */
 int main(void)
 {
-	fprintf(putchar);
-	return 0;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
