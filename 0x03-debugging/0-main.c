60d05c1e69a2662be700af99dbd277c5720d50bf
collins <kevin1520smart@gmail.com>
2023-02-23 10:42:48 -0800

    debugging section

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Test for positve or negative
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	1 = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN.H
+#define MAIN.h
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+int largest_number(int a, int b, int c);
+
+void print_remaining_days(int month, int day, int year);
+
+int covert_day(int month, int day);
+
+#endif
