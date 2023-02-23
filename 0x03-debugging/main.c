d06f311955a73c6a01f06a0cdd8e6c760927e574
collins <kevin1520smart@gmail.com>
2023-02-23 10:53:11 -0800

    debugging section(2)

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - test function that prints positive or negative values
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+
+	positive_or_negative(i);
+	return (0);
+}
