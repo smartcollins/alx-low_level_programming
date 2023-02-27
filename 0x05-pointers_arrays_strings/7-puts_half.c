11cdf19d53bede133a975040d520e471978c758b
collins <kevin1520smart@gmail.com>
2023-02-27 12:57:09 -0800

    7-Winning is only half of it(3)

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,12 +14,15 @@ void puts_half(char *str)
 	for (a = 0; str[a] != '\0'; a++)
 	{
 		c++;
-		b = (c / 2);
-		if ((c % 2) == 1)
-			b = ((c + 1) / 2);
-
-		for (a = b; str[a] != '\0'; a++)
-			_putchar(str[a]);
-		_putchar('\n');
 	}
+	b = (c / 2);
+		if ((c % 2) == 1)
+		{
+			b = ((c + 1) / 2);
+		}
+		for (a = b; str[a] != '\0'; a++)
+		{
+			_putchar(str[a]);
+		}
+	_putchar('\n');
 }
