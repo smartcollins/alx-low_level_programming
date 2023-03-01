3e14364307cfbbf692afa1457a6ffdcfefba51bb
collins <kevin1520smart@gmail.com>
2023-03-01 15:13:20 -0800

    6. Expect the best.

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words
+ * @z: value
+ * Return: z
+ */
+char *cap_string(char *z)
+{
+	int a = 0;
+
+	while (z[a])
+	{
+		while (!(z[a] <= 'z') && !(z[a] >= 'a'))
+			a++;
+		if ((z[a - 1] == '') || (z[a - 1] == "") ||
+		    (z[a - 1] == '\t') || (z[a - 1] == '\n') ||
+		    (z[a - 1] == ',') || (z[a - 1] == '.') ||
+		    (z[a - 1] == '{') || (z[a - 1] == '}') ||
+		    (z[a - 1] == '(') || (z[a - 1] == ')') ||
+		    (z[a - 1] == '?') || (z[a - 1] == '!') ||)
+		{
+			z[a] -= 32;
+		}
+		a++;
+	}
+	return (z);
+}
