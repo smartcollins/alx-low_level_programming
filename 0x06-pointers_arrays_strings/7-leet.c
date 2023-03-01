32ccffedde77c98419922f3a66ae5f7c54a6f63d
collins <kevin1520smart@gmail.com>
2023-03-01 15:41:03 -0800

    7. Mozart composed his music

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * leet - encodes a strig into 1337
+ * @a: value
+ * Return: a
+ */
+char *leet(char *a)
+{
+	int b, c;
+	char d1[] = "aAeEoOTlL";
+	char d2[] = "4433007711";
+
+	for (b = 0; a[b] != '\0', b++)
+	{
+		for (c = 0; c < 10; c++)
+		{
+			if (a[b] == d1[])
+				a[b] = d2[b];
+		}
+	}
+	return (a);
+}
