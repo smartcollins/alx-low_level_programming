30ac501fb8fb186a0f31a6a29d2e9f443f3e99c5
collins <kevin1520smart@gmail.com>
2023-03-03 12:59:22 -0800

    9. Numbers have life

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: value
+ */
+void print_number(int n)
+{
+	unsigned int a;
+
+	a = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		a = -n;
+	}
+	if (a / 10 != 0)
+		print_number(a / 10);
+
+	_putchar((a % 10) + '0');
+}
